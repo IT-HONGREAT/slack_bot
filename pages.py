04e156063d634a22571bb5396b9addcd5e44552d
hong-in-yeong <hongreat95@gmail.com>
2023-01-30 16:10:47 +0900

    post pages in db

--- pages.py
+++ pages.py
@@ -1,60 +1,191 @@
 {
-    "object": "database",
-    "id": "486b0c63-9daf-40d8-9ba2-538e7214e47f",
-    "cover": null,
-    "icon": null,
-    "created_time": "2023-01-30T05:25:00.000Z",
-    "created_by": {"object": "user", "id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8"},
-    "last_edited_by": {"object": "user", "id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8"},
-    "last_edited_time": "2023-01-30T05:51:00.000Z",
-    "title": [
+    "has_more": False,
+    "next_cursor": None,
+    "object": "list",
+    "page": {},
+    "results": [
         {
-            "type": "text",
-            "text": {"content": "테스트 테이블", "link": null},
-            "annotations": {
-                "bold": false,
-                "italic": false,
-                "strikethrough": false,
-                "underline": false,
-                "code": false,
-                "color": "default",
+            "archived": False,
+            "cover": None,
+            "created_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "created_time": "2023-01-30T05:25:00.000Z",
+            "icon": None,
+            "id": "3ddf4184-12a2-44ad-bd7e-d41dc0e4d0a8",
+            "last_edited_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "last_edited_time": "2023-01-30T05:50:00.000Z",
+            "object": "page",
+            "parent": {"database_id": "486b0c63-9daf-40d8-9ba2-538e7214e47f", "type": "database_id"},
+            "properties": {
+                "방": {
+                    "id": "sr%3B%5E",
+                    "select": {"color": "green", "id": "dd6e5fce-0857-43f2-8fae-27db85c6af41", "name": "지니"},
+                    "type": "select",
+                },
+                "생성자": {
+                    "created_by": {
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/dee2d8d7-19f0-4f24-91f6-32b0016bf033/017C2446-69AE-4F5F-A048-884EF18EDF23.png",
+                        "id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8",
+                        "name": "홍인영",
+                        "object": "user",
+                        "person": {"email": "iyiy95@naver.com"},
+                        "type": "person",
+                    },
+                    "id": "GH%5CG",
+                    "type": "created_by",
+                },
+                "이름": {
+                    "id": "title",
+                    "title": [
+                        {
+                            "annotations": {
+                                "bold": False,
+                                "code": False,
+                                "color": "default",
+                                "italic": False,
+                                "strikethrough": False,
+                                "underline": False,
+                            },
+                            "href": None,
+                            "plain_text": "test3",
+                            "text": {"content": "test3", "link": None},
+                            "type": "text",
+                        }
+                    ],
+                    "type": "title",
+                },
+                "이용시간": {"date": None, "id": "%3ArOl", "type": "date"},
+                "태그": {
+                    "id": "Q%5CMT",
+                    "select": {"color": "gray", "id": "f7d3d89d-869d-4687-a199-f5f430b7122e", "name": "기타"},
+                    "type": "select",
+                },
             },
-            "plain_text": "테스트 테이블",
-            "href": null,
-        }
+            "url": "https://www.notion.so/test3-3ddf418412a244adbd7ed41dc0e4d0a8",
+        },
+        {
+            "archived": False,
+            "cover": None,
+            "created_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "created_time": "2023-01-30T05:25:00.000Z",
+            "icon": None,
+            "id": "6f3d609f-5e4e-4f77-8641-9ca2a3583871",
+            "last_edited_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "last_edited_time": "2023-01-30T05:50:00.000Z",
+            "object": "page",
+            "parent": {"database_id": "486b0c63-9daf-40d8-9ba2-538e7214e47f", "type": "database_id"},
+            "properties": {
+                "방": {
+                    "id": "sr%3B%5E",
+                    "select": {"color": "green", "id": "dd6e5fce-0857-43f2-8fae-27db85c6af41", "name": "지니"},
+                    "type": "select",
+                },
+                "생성자": {
+                    "created_by": {
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/dee2d8d7-19f0-4f24-91f6-32b0016bf033/017C2446-69AE-4F5F-A048-884EF18EDF23.png",
+                        "id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8",
+                        "name": "홍인영",
+                        "object": "user",
+                        "person": {"email": "iyiy95@naver.com"},
+                        "type": "person",
+                    },
+                    "id": "GH%5CG",
+                    "type": "created_by",
+                },
+                "이름": {
+                    "id": "title",
+                    "title": [
+                        {
+                            "annotations": {
+                                "bold": False,
+                                "code": False,
+                                "color": "default",
+                                "italic": False,
+                                "strikethrough": False,
+                                "underline": False,
+                            },
+                            "href": None,
+                            "plain_text": "test1",
+                            "text": {"content": "test1", "link": None},
+                            "type": "text",
+                        }
+                    ],
+                    "type": "title",
+                },
+                "이용시간": {
+                    "date": {
+                        "end": "2023-01-30T16:00:00.000+09:00",
+                        "start": "2023-01-30T00:00:00.000+09:00",
+                        "time_zone": None,
+                    },
+                    "id": "%3ArOl",
+                    "type": "date",
+                },
+                "태그": {
+                    "id": "Q%5CMT",
+                    "select": {"color": "orange", "id": "1e5bd3cc-0de2-41c9-84d4-5bd58d060918", "name": "내부회의"},
+                    "type": "select",
+                },
+            },
+            "url": "https://www.notion.so/test1-6f3d609f5e4e4f7786419ca2a3583871",
+        },
+        {
+            "archived": False,
+            "cover": None,
+            "created_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "created_time": "2023-01-30T05:25:00.000Z",
+            "icon": None,
+            "id": "ae46e4db-6075-4997-a188-7fdaece21ea0",
+            "last_edited_by": {"id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8", "object": "user"},
+            "last_edited_time": "2023-01-30T05:50:00.000Z",
+            "object": "page",
+            "parent": {"database_id": "486b0c63-9daf-40d8-9ba2-538e7214e47f", "type": "database_id"},
+            "properties": {
+                "방": {
+                    "id": "sr%3B%5E",
+                    "select": {"color": "orange", "id": "481c210b-9ad2-4682-b74a-6db42d717e97", "name": "어스"},
+                    "type": "select",
+                },
+                "생성자": {
+                    "created_by": {
+                        "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/dee2d8d7-19f0-4f24-91f6-32b0016bf033/017C2446-69AE-4F5F-A048-884EF18EDF23.png",
+                        "id": "f0aa6e91-dd70-4765-a5b7-ff63d9f41be8",
+                        "name": "홍인영",
+                        "object": "user",
+                        "person": {"email": "iyiy95@naver.com"},
+                        "type": "person",
+                    },
+                    "id": "GH%5CG",
+                    "type": "created_by",
+                },
+                "이름": {
+                    "id": "title",
+                    "title": [
+                        {
+                            "annotations": {
+                                "bold": False,
+                                "code": False,
+                                "color": "default",
+                                "italic": False,
+                                "strikethrough": False,
+                                "underline": False,
+                            },
+                            "href": None,
+                            "plain_text": "test2",
+                            "text": {"content": "test2", "link": None},
+                            "type": "text",
+                        }
+                    ],
+                    "type": "title",
+                },
+                "이용시간": {"date": None, "id": "%3ArOl", "type": "date"},
+                "태그": {
+                    "id": "Q%5CMT",
+                    "select": {"color": "brown", "id": "3ec4a390-ac5c-4546-a55f-5f5215de0a84", "name": "미팅"},
+                    "type": "select",
+                },
+            },
+            "url": "https://www.notion.so/test2-ae46e4db60754997a1887fdaece21ea0",
+        },
     ],
-    "description": [],
-    "is_inline": true,
-    "properties": {
-        "이용시간": {"id": "%3ArOl", "name": "이용시간", "type": "date", "date": {}},
-        "생성자": {"id": "GH%5CG", "name": "생성자", "type": "created_by", "created_by": {}},
-        "태그": {
-            "id": "Q%5CMT",
-            "name": "태그",
-            "type": "select",
-            "select": {
-                "options": [
-                    {"id": "c2814c42-ce8c-417e-b22f-72d4a37b2b6f", "name": "회의", "color": "pink"},
-                    {"id": "1e5bd3cc-0de2-41c9-84d4-5bd58d060918", "name": "내부회의", "color": "orange"},
-                    {"id": "3ec4a390-ac5c-4546-a55f-5f5215de0a84", "name": "미팅", "color": "brown"},
-                    {"id": "f7d3d89d-869d-4687-a199-f5f430b7122e", "name": "기타", "color": "gray"},
-                ]
-            },
-        },
-        "방": {
-            "id": "sr%3B%5E",
-            "name": "방",
-            "type": "select",
-            "select": {
-                "options": [
-                    {"id": "dd6e5fce-0857-43f2-8fae-27db85c6af41", "name": "지니", "color": "green"},
-                    {"id": "481c210b-9ad2-4682-b74a-6db42d717e97", "name": "어스", "color": "orange"},
-                ]
-            },
-        },
-        "이름": {"id": "title", "name": "이름", "type": "title", "title": {}},
-    },
-    "parent": {"type": "page_id", "page_id": "03a14bc7-ba58-486c-92f0-57175782f46b"},
-    "url": "https://www.notion.so/486b0c639daf40d89ba2538e7214e47f",
-    "archived": false,
+    "type": "page",
 }
