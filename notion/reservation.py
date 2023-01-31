c2874118d7205e34312eb6f848512858c9901d27
hong-in-yeong <hongreat95@gmail.com>
2023-01-31 10:53:47 +0900

    setting class clean

--- notion/reservation.py
+++ notion/reservation.py
@@ -1,22 +1,17 @@
 import requests, json
 
 from envs import get_env
+from settings import PlatFormSetting
 
 
-class PlatForm:
-    def __init__(self, token=None):
-        self.token = token
-
-
-class Notion(PlatForm):
-    def __init__(self, notion_db_name):
+class Notion(PlatFormSetting):
+    def __init__(self):
         super().__init__()
-
+        # print("???!?!?!", self.token)
         # self.db_name = get_env().get(f"{notion_db_name}")
         # self.Notion = get_env().get("Notion")
-
         self.headers = {
-            "Authorization": "Bearer " + self.Notion,
+            "Authorization": "Bearer " + self.token,
             "accept": "application/json",
             "Notion-Version": "2022-06-28",
             "content-type": "application/json",
@@ -26,70 +21,76 @@ class Notion(PlatForm):
 
         return
 
-    def get_reservation(databaseId, headers):
-        readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
 
-        response = requests.post(readUrl, headers=headers)
-        data = response.json()
-        data_result = data.get("results")
-        for i in data_result:
-            one_property = i.get("properties")
+test = Notion()
+print("testsett", test.token)
 
-            room = one_property["방"]["select"]["name"]
-            created_by = one_property["생성자"]["created_by"]["name"]
-            title = one_property["제목"]["title"][0].get("plain_text")
-            if one_property["이용시간"].get("date"):
-                start = one_property["이용시간"].get("date").get("start")
-                end = one_property["이용시간"].get("date").get("end")
-            tag = one_property["용도"]["select"]["name"]
 
-            print("방", "=>", room)
-            print("생성자", "=>", created_by)
-            print("제목", "=>", title)
-            if one_property["이용시간"].get("date"):
-                print("이용시간", "=>", start)
-                print("이용시간", "=>", end)
-            print("용도", "=>", tag)
+def get_reservation(databaseId, headers):
+    readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
 
-        return {"status_code": response.status_code}
+    response = requests.post(readUrl, headers=headers)
+    data = response.json()
+    data_result = data.get("results")
+    for i in data_result:
+        one_property = i.get("properties")
 
-    def create_reservation(
-        databaseId,
-        headers,
-        room=None,
-        title=None,
-        purpose=None,
-        start=None,
-        end=None,
-    ):
-        createdUrl = "https://api.notion.com/v1/pages"
+        room = one_property["방"]["select"]["name"]
+        created_by = one_property["생성자"]["created_by"]["name"]
+        title = one_property["제목"]["title"][0].get("plain_text")
+        if one_property["이용시간"].get("date"):
+            start = one_property["이용시간"].get("date").get("start")
+            end = one_property["이용시간"].get("date").get("end")
+        tag = one_property["용도"]["select"]["name"]
 
-        newPageData = {
-            "parent": {"database_id": databaseId},
-            "properties": {
-                "방": {
-                    "select": {
-                        "name": room,
-                    },
-                },
-                # "생성자": {
-                #     "created_by": {
-                #         "name": "홍인영",
-                #         "object": "user",
-                #         "person": {"email": "iyiy95@naver.com"},
-                #     },
-                #     "type": "created_by",
-                # },
-                "제목": {"title": [{"text": {"content": title}}]},
-                "이용시간": {"date": {"start": start, "end": end}},
-                "용도": {
-                    "select": {
-                        "name": purpose,
-                    }
+        print("방", "=>", room)
+        print("생성자", "=>", created_by)
+        print("제목", "=>", title)
+        if one_property["이용시간"].get("date"):
+            print("이용시간", "=>", start)
+            print("이용시간", "=>", end)
+        print("용도", "=>", tag)
+
+    return {"status_code": response.status_code}
+
+
+def create_reservation(
+    databaseId,
+    headers,
+    room=None,
+    title=None,
+    purpose=None,
+    start=None,
+    end=None,
+):
+    createdUrl = "https://api.notion.com/v1/pages"
+
+    newPageData = {
+        "parent": {"database_id": databaseId},
+        "properties": {
+            "방": {
+                "select": {
+                    "name": room,
                 },
             },
-        }
+            # "생성자": {
+            #     "created_by": {
+            #         "name": "홍인영",
+            #         "object": "user",
+            #         "person": {"email": "iyiy95@naver.com"},
+            #     },
+            #     "type": "created_by",
+            # },
+            "제목": {"title": [{"text": {"content": title}}]},
+            "이용시간": {"date": {"start": start, "end": end}},
+            "용도": {
+                "select": {
+                    "name": purpose,
+                }
+            },
+        },
+    }
 
-        data = json.dumps(newPageData)
-        response = requests.post(createdUrl, headers=headers, data=data)
-        return {"status_code": response.status_code}
+    data = json.dumps(newPageData)
+    response = requests.post(createdUrl, headers=headers, data=data)
+    return {"status_code": response.status_code}
--- settings.py
+++ settings.py
@@ -5,20 +5,14 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 class PlatFormSetting:
-    def __init__(self, temp_purpose=None):
-        self.token = self._get_secret(temp_purpose)
+    def __init__(self):
+        self.token = self._get_secret()
 
-    def _get_secret(self, temp_purpose):
-
+    @classmethod
+    def _get_secret(cls):
+        print("cls~!!", cls.__name__)
         check = dotenv.get_key(
             dotenv_path=f"{BASE_DIR}/.env",
-            key_to_get=f"{temp_purpose}",
+            key_to_get=f"{cls.__name__}",
         )
         return check
-
-
-test = PlatFormSetting(temp_purpose="Notion")
-db = PlatFormSetting(temp_purpose="reservation_db")
-
-print("!!", test.token)
-print("dbdbd", db.token)
