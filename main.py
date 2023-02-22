c72d34d72985b81a5d1720c2afd8eca966bd6ae7
hong-in-yeong <hongreat95@gmail.com>
2023-02-22 12:50:28 +0900

    #18 Add process running exception

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from pathlib import Path
 from bolt.actions import bolt_socket
 
 BASE_DIR = Path(__file__).resolve().parent
-# TODO Improve Process.
 
 
 def main(filename):
-    subprocess.call(["python", filename])
+    """
+    Runs the Python script with the given filename
+    """
+    try:
+        subprocess.run(["python", str(filename)], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {filename}: {e}")
 
 
 if __name__ == "__main__":
     bolt_socket()
-    main(f"{BASE_DIR}/bolt/main.py")
+    main(BASE_DIR / "bolt" / "main.py")
