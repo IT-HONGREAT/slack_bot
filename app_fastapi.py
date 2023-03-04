3a3d74f7107dc7cd217f4e5dce158e2e1177290a
hong-in-yeong <hongreat95@gmail.com>
2023-03-04 18:07:36 +0900

    📚 Docs: #23 doc_apirouter todo

--- app_fastapi.py
+++ app_fastapi.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 from slack_bolt.adapter.asgi import SlackRequestHandler
 
 from bolt.app import bolt_app
 from bolt.utils import get_user_id
 from router_fastapi.models import Alarm
 
+# TODO
+"""
+
+fastapi의 apirouter 적용으로 flask의 blueprint처럼 활용가능한지 확인.
+
+fastapi call 시 slack app call 되는점을 이용할것.
+    slack api call 시 event handle이 안되는 점 파악하고 개선할것. 
+
+"""
+
+
 api = FastAPI()
 app_handler = SlackRequestHandler(bolt_app)
+
+# api_router = APIRouter()
 
 
 @api.post("/alarm")
