d0a049c1c2ead01514d66ec29dea3f73d176dfb8
hong-in-yeong <hongreat95@gmail.com>
2023-03-06 10:44:33 +0900

    🔨 Refactor: #23 api router

--- app_fastapi.py
+++ app_fastapi.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi import FastAPI
 from slack_bolt.adapter.asgi import SlackRequestHandler
 
 from bolt.app import bolt_app
-from bolt.utils import get_user_id
-from router_fastapi.models import Alarm
+from router_fastapi import apis
 
 # TODO
 """
@@ -18,19 +17,7 @@ fastapi call 시 slack app call 되는점을 이용할것.
 
 api = FastAPI()
 app_handler = SlackRequestHandler(bolt_app)
-
-# api_router = APIRouter()
-
-
-@api.post("/alarm")
-async def alarm(alarm: Alarm):
-
-    check = bolt_app.client.users_list()
-    user_id = get_user_id(check, user_email=alarm.user_email)
-    if user_id:
-        bolt_app.client.chat_postMessage(channel=user_id, text=f"{alarm.user_name}님이 {alarm.context}관련 내용을 입력했습니다.")
-        return {"user_id": user_id}
-    raise HTTPException(status_code=404, detail="유저를 찾을수 없습니다.")
+api.include_router(apis.router)
 
 
 # uvicorn 모듈이름:api --reload --port 3000 --log-level warning
