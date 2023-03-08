bdafc4a4a5a0baf7991770f3dcad7c067fb04ef9
hong-in-yeong <hongreat95@gmail.com>
2023-03-08 16:50:57 +0900

    ✨ Feat: 14 contract settlement for slack alarm

--- router_fastapi/apis.py
+++ router_fastapi/apis.py
@@ -1,5 +1,5 @@
+from asyncio import sleep
 from datetime import datetime
-from typing import List
 
 from fastapi import APIRouter, HTTPException
 
@@ -23,31 +23,85 @@ async def alarm(alarm: Alarm):
 
 
 @router.post("/contract_settlement_alarm")
-async def contract_management_alarm(contract_settlement: List[ContractSettlement]):
+async def contract_management_alarm(contract_settlement: ContractSettlement):
+    users = bolt_app.client.users_list()
+    await sleep(1)
 
-    user_id = get_user_information(bolt_app.client.users_list(), client_user_email=contract_settlement.user_email)
+    today_check = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"{i.today_contract_management_project_name}] {i.today_payment_kind} (₩{'{:,}'.format(i.today_amount)}",
+            },
+        }
+        for i in contract_settlement.today_alarm_projects
+    ]
 
+    next_check = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"{i.next_week_paid_date} [{i.next_week_contract_management_project_name}] {i.next_week_payment_kind} (₩{'{:,}'.format(i.next_week_amount)})",
+            },
+        }
+        for i in contract_settlement.next_week_projects
+    ]
+    delay_check = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"[{i.delay_contract_management_project_name}] (₩{'{:,}'.format(i.delay_amount)}) : {i.delay_reason} )",
+            },
+        }
+        for i in contract_settlement.delay_projects
+    ]
+
+    user_id = get_user_information(users, client_user_email=contract_settlement.user_email)
+    user_name = get_user_information(users, client_user_id=user_id)
     today = datetime.now().strftime("%Y년 %m월 %d일")
     if user_id:
-        # bolt_app.client.chat_postMessage(
-        #     channel=user_id,
-        #     blocks=[
-        #         {
-        #             "type": "section",
-        #             "text": {
-        #                 "type": "mrkdwn",
-        #                 "text": f"안녕하세요 ^^ {contract.user_email}님!! {today} 정산일정 공유드립니다."
-        #                 f"*[금일 정산 예정 프로젝트]* ₩{contract.today_amount}"
-        #                 f"{contract.today_contract_management_project_name} 잔금 ₩{contract.today_amount}"
-        #                 f"{contract.today_payment_kind}"
-        #                 f"*[차주 전체 정산 예정 프로젝트]* ₩{contract.next_week_amount}"
-        #                 f"{contract.next_week_paid_date} {contract.next_week_contract_management_project_name} 선금(₩{{contract.next_week_amount}})"
-        #                 f"{contract.next_week_payment_kind}"
-        #                 f"*[지급 지연 프로젝트]* 총 ₩{contract.delay_amount}(Todo)"
-        #                 f"* {contract.delay_contract_management_project_name} 잔금 ₩{contract.delay_amount} : {contract.delay_reason}",
-        #             },
-        #         }
-        #     ],
-        # )
+        bolt_app.client.chat_postMessage(
+            channel=user_id,
+            blocks=[
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"안녕하세요 :wave: {user_name}님!! {today} 정산일정 공유드립니다.",
+                        "emoji": True,
+                    },
+                },
+                {"type": "divider"},
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f":one:*[금일 예정 프로젝트]*\n",
+                    },
+                },
+                *today_check,
+                {"type": "divider"},
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f":two:*[차주 정산 예정 프로젝트]*\n",
+                    },
+                },
+                *next_check,
+                {"type": "divider"},
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f":three:*[지급 지연 프로젝트]*\n",
+                    },
+                },
+                *delay_check,
+            ],
+        )
         return {"user_id": user_id}
     raise HTTPException(status_code=404, detail="유저를 찾을수 없습니다.")
